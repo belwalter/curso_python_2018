a42d17526e655bd37b66ef163ae789c96e3473eb
belwalter <belwalterv@gmail.com>
2018-06-28 09:38:30 -0300

    clase 5

--- clase4.py
+++ clase4.py
@@ -7,11 +7,13 @@ class persona():
     def __init__(self, nombre=None, apellido=None, hermano=None):
         self.__nombre = nombre
         self.__apellido = apellido
-        self.__hermano = hermano
+        self.__hermano = []
+
 
     @property
     def nombre(self):
         return self.__nombre
+
     @nombre.setter
     def nombre(self, nombre):
         self.__nombre = nombre.capitalize()
@@ -23,27 +25,41 @@ class persona():
     def __str__(self):
         return self.__nombre + ' ' + self.__apellido
 
-    def get_hno(self):
+    def get_lista_hno(self):
         return self.__hermano
+
+    def set_hno(self, hermano):
+        self.__hermano.append(hermano)
 
 # nombre = input('ingrese nombre')
 
 
 p = persona('walter', 'bel')
-p1 = persona('asdasd', 'asdasd', p)
+p1 = persona('asdasd', 'asdasd')
+p2 = persona('ffff', 'fff')
+p3 = persona('rrrr', 'rrr')
+
+p.set_hno(p1)
+p.set_hno(p2)
+p.set_hno(p3)
+
+for hermano in p.get_lista_hno():
+    print(hermano)
 
 lista = [p, p1]
 p.nombre = "nuevo"
 print(p.nombre)
 
+
 def orden(elemento):
     return elemento.get_apellido() + elemento.get_nombre()
+
 
 '''
 lista.sort(key=orden)
 for elemento in lista:
     print(elemento)
 '''
-#print(p1.get_hno().set_nombre('Lucas'))
+# print(p1.get_hno().set_nombre('Lucas'))
 
-#print(p.get_nombre())
+# print(p.get_nombre())
---
+++ clase5.py
@@ -0,0 +1,118 @@
+
+
+class Animal():
+
+    def __init__(self):
+        self.__nombre = None
+
+    def comer(self):
+        return "el animal come"
+
+    def get_nombre(self):
+        return self.__nombre
+
+
+class Perro(Animal):
+
+    def __init__(self):
+        self.color_pelo = None
+
+    def comer(self):
+        return "el perro come"
+
+
+class Paloma(Perro):
+
+    def __init__(self):
+        self.vuela = None
+
+    def comer(self):
+        return "la paloma come"
+
+
+obj1 = Paloma()
+obj2 = Perro()
+obj3 = Animal()
+
+lista = [obj1, obj2, obj3]
+
+for elemento in lista:
+    print(elemento.comer())
+    #if (isinstance(elemento, Perro)):
+    #    print(elemento.comer2())
+
+"""
+
+class Animal(object):
+    def desplazarse(self):
+        print("el animal se mueve")
+
+class Terrestre(object):
+
+    def desplazarse(self):
+        print("el animal camina")
+
+
+class Acuatico(Animal):
+
+    def desplazarse2(self):
+        print("el animal nada")
+
+
+class Cocodrilo(Acuatico, Terrestre):
+
+    def __init__(self):
+        pass
+
+    def desplazarse(self):
+        #Animal.desplazarse(self)
+        #Terrestre.desplazarse(self)
+        #Acuatico.desplazarse(self)
+        super().desplazarse()
+        print("el animal nada o camina depende del medio")
+
+
+a = Cocodrilo()
+a.desplazarse()
+
+
+from abc import ABCMeta
+import abc
+
+class forma(metaclass=ABCMeta):
+
+    __color = None
+
+    @abc.abstractmethod
+    def calcular_area(self):
+        pass
+
+    #@abc.abstractmethod
+    def get_color(self):
+        return self.__color
+
+
+class circulo(forma):
+
+    def __init__(self, radio, color=None):
+        self.__color = color
+        self.__radio = radio
+
+    def calcular_area(self):
+        return 3.14 * (self.__radio**2)
+
+a = circulo(12)
+
+print(a.calcular_area())
+
+class rectangulo(forma):
+
+    def __init__(self, lado1, lado2, color=None):
+        self.__lado1 = lado1
+        self.__lado2 = lado2
+        self.__color = color
+
+    def calcular_area(self):
+        return self.__lado1 * self.__lado2
+
+"""
